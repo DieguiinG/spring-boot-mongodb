884a462166fa3795502bf2ef971238cf1ce82129
DieguiinG <difejacyntho@hotmail.com>
2023-03-22 16:49:09 -0300

    Projeto Criado

---
+++ src/test/java/com/diegog/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegog.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
