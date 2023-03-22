a7f5ee48fad75802ae402585c40f98709be482a7
DieguiinG <difejacyntho@hotmail.com>
2023-03-22 19:44:31 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/diegog/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.diegog.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.diegog.workshopmongo.domain.User;
+import com.diegog.workshopmongo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+
+}
