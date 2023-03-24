4fae79306a2a03fcf450f0e8b3c004866320dc4f
DieguiinG <difejacyntho@hotmail.com>
2023-03-24 18:28:46 -0300

    Inserindo usuario com post

--- src/main/java/com/diegog/workshopmongo/resources/UserResource.java
+++ src/main/java/com/diegog/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.diegog.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.diegog.workshopmongo.domain.User;
 import com.diegog.workshopmongo.dto.UserDTO;
@@ -27,10 +30,20 @@ public class UserResource {
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	@GetMapping(value="{id}")
+
+	@GetMapping(value = "{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
-		
+
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@RequestMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
+		User obj = service.fromDTO(objDTO);
+		obj = service.Insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+
 	}
 }
--- src/main/java/com/diegog/workshopmongo/services/UserService.java
+++ src/main/java/com/diegog/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.diegog.workshopmongo.domain.User;
+import com.diegog.workshopmongo.dto.UserDTO;
 import com.diegog.workshopmongo.repositories.UserRepository;
 import com.diegog.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,10 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-
+	public User Insert(User obj){
+		return repo.insert(obj);
+	}
+	public User fromDTO(UserDTO objDTO){
+		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
+	}
 }
