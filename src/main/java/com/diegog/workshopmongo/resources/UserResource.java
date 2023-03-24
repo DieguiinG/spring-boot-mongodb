92b261fe487d2d89fb6415d22fc1d5548b93d9a8
DieguiinG <difejacyntho@hotmail.com>
2023-03-24 18:39:06 -0300

    Deletando usuario com delete

--- src/main/java/com/diegog/workshopmongo/resources/UserResource.java
+++ src/main/java/com/diegog/workshopmongo/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -45,5 +46,13 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 
+	}
+
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<UserDTO> delete(@PathVariable String id) {
+		User obj = service.findById(id);
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/diegog/workshopmongo/services/UserService.java
+++ src/main/java/com/diegog/workshopmongo/services/UserService.java
@@ -13,23 +13,30 @@ import com.diegog.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	public User findById(String id){
+
+	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	public User Insert(User obj){
+
+	public User Insert(User obj) {
 		return repo.insert(obj);
+
 	}
-	public User fromDTO(UserDTO objDTO){
-		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
+
+	public void delete(String id) {
+		 findById(id);
+		 repo.deleteById(id);
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 }
