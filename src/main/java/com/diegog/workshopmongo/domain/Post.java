ea5dd45823f53d60d87feaffcdf8447549f0d991
DieguiinG <difejacyntho@hotmail.com>
2023-03-24 23:40:34 -0300

    acrescentando comentarios aos posts

--- src/main/java/com/diegog/workshopmongo/domain/Post.java
+++ src/main/java/com/diegog/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.diegog.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.diegog.workshopmongo.dto.AuthorDTO;
+import com.diegog.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 
@@ -90,6 +95,14 @@ public class Post implements Serializable {
 		return Objects.equals(author, other.author) && Objects.equals(body, other.body)
 				&& Objects.equals(date, other.date) && Objects.equals(id, other.id)
 				&& Objects.equals(title, other.title);
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 }
